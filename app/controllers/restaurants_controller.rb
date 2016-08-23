d71eb6f53c163f9be0daed1c5f1de773d547e240
ogirginc <ogirginc@gmail.com>
2016-08-23 11:45:52 +0100

    WIP: Challenge 8

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,13 @@
 class RestaurantsController < ApplicationController
   def index
-   @restaurants = Restaurant.all
+    @restaurants = Restaurant.all
+  end
+
+  def new
+  end
+
+  def create
+    Restaurant.create(params[:restaurant])
+    redirect_to '/restaurants'
   end
 end
