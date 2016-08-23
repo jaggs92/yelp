813c4596f20cb349b433d595d1a220e15be81eee
Joe Sweeny <joesweeny84@hotmail.com>
2016-08-23 17:59:42 +0100

    V2 Ch1 - Setup Devise

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
 resources :restaurants do
   resources :reviews
 end
+
+root "restaurants#index"
 
 
 # get 'restaurants' => 'restaurants#index'
