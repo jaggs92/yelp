aaf39ee17681466a3042e40e1bdadf8f85de50e9
Harry Jaggs <harryjaggs92@gmail.com>
2016-08-23 16:53:28 +0100

    validates

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -7,4 +7,10 @@ describe Restaurant, type: :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: "Nando's")
+    restaurant = Restaurant.new(name: "Nando's")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
