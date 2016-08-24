7aadc61e0534adba35d6141e52fa85e7671bd75d
Harry Jaggs <harryjaggs92@gmail.com>
2016-08-24 21:01:01 +0100

    sorting out directory

---
+++ spec/features/restaurants_features_spec.rb
@@ -0,0 +1,89 @@
+require 'rails_helper'
+require 'web_helper'
+
+feature 'restaurants' do
+  context 'no restaurants have been added' do
+    scenario 'should display a prompt to add restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content 'No restaurants yet'
+      expect(page).to have_link 'Add a restaurant'
+    end
+  end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'displays restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+  end
+
+  context 'creating restaurants' do
+    scenario 'should be able to add a restaurant' do
+      sign_up
+      visit '/restaurants'
+      click_link "Add a restaurant"
+      fill_in "Name", with: "KFC"
+      click_button "Create Restaurant"
+      require 'pry'; binding.pry
+      expect(current_path).to eq "/restaurants"
+      expect(page).to have_content "KFC"
+    end
+
+    context 'an invalid restaurant' do
+      it 'does not let you submit a name that is too short' do
+        sign_up
+        visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'kf'
+        click_button 'Create Restaurant'
+        expect(page).not_to have_css 'h2', text: 'kf'
+        expect(page).to have_content 'error'
+      end
+    end
+  end
+
+  context 'viewing restaurants' do
+    let!(:kfc) { Restaurant.create(name: 'KFC')}
+
+    scenario 'lets a user view a restaurant' do
+      visit '/restaurants'
+      click_link 'KFC'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq "/restaurants/#{kfc.id}"
+    end
+  end
+
+  context 'editing restaurants' do
+    before { Restaurant.create(name: 'KFC', description: 'Mmmm, love that skin') }
+
+    scenario 'lets users edit restaurants' do
+      sign_up
+      visit '/restaurants'
+      click_link "Edit KFC"
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      fill_in 'Description', with: 'Deep fried goodness'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Deep fried goodness'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'deleting restaurants' do
+    before { Restaurant.create(name: 'KFC', description: 'Deep fried goodness')}
+
+    scenario 'removes a restaurant when a user clicks a link' do
+      sign_up
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
+
+end
